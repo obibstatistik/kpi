05c97bd208d9bb5ae1c68ac365c5dbae84966be7
kentgottschalk <kentgottschalk@gmail.com>
2018-05-09 13:53:30 +0200

    bugfix

--- sections/meetingrooms.R
+++ sections/meetingrooms.R
@@ -78,7 +78,7 @@ meetingroomsTabPanel <- function(input, output, session, data, tablename) {
     )
 
   Nweekdays <- Vectorize(function(a, b) 
-    sum(!weekdays(seq(a, b, "days")) %in% c("Saturday", "Sunday", "Lørdag", "Søndag" )))
+    sum(!weekdays(seq(a, b, "days")) %in% c("Saturday", "Sunday", "lørdag", "søndag" )))
   
   output$tablemeetingrooms_overview <- renderTable(
     meetingrooms_overview <- meetingrooms %>%
