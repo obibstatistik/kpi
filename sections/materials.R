e34ed5c0d6c0d47448b603775d07d63a5589e3c2
sorenb <soeb@odense.dk>
2018-06-16 11:45:16 +0200

    Made samedate_barchart work for checkouts/loans

--- sections/materials.R
+++ sections/materials.R
@@ -23,14 +23,24 @@ materialsTabPanelUI <- function(id) {
                                    fluidRow(
                                      column(2, h4("Afgræns")),
                                      column(10,
-                                            plotlyOutput(ns("heat")),
-                                            tableOutput(ns('table'))
+                                            h4("Samlet udlån på OBB"),
+                                            p("Farvet: fra 1. januar til dags dato i pågældende år. Grå: Året total"),
+                                            samedate_barchartOutput(ns('checkouts_samedate_plot'))
                                      ),
                                      column(12,
                                             formattableOutput(ns("checkout_all_plot"))
                                      ),
                                      column(12,
                                             formattableOutput(ns("checkout_all_table")))
+                                   )
+                          ),
+                          tabPanel("Timer", 
+                                   fluidRow(
+                                     column(2, h4("Afgræns")),
+                                     column(10,
+                                            plotlyOutput(ns("heat")),
+                                            tableOutput(ns('table'))
+                                     )
                                    )
                           ),
                           tabPanel("Cirkulation", 
@@ -74,9 +84,55 @@ materialsTabPanel <- function(input, output, session, data, tablename) {
   drv <- dbDriver("PostgreSQL")
   con <- dbConnect(drv, dbname = dbname, host = host, port = port, user = user, password = password)
   udlaan <- dbGetQuery(con, "SELECT name, hour, circulation_fact_count FROM cicero.udlaan_per_klokkeslaet")
-  checkout_all <- dbGetQuery(con, "SELECT * FROM public.people_counter")
+  max_date <- dbGetQuery(con, "select max(transact_date) max_date from cicero.udlaan_per_opstillingsprofil")
+  checkouts_all <- dbGetQuery(con, "SELECT extract(year from transact_date) aar,transact_date, sum(antal) antal
+    from cicero.udlaan_per_opstillingsprofil
+    where extract(year from (transact_date)) > extract(year from (current_date - interval '5 year'))
+    group by aar,transact_date
+    order by aar,transact_date")
   dbDisconnect(con)
   
+  # Calculate latest date with data
+  max_date <- checkouts_all %>%
+    summarize(max_date = max(transact_date))
+  
+  # Calculate sums for partial years, adapting max_date to each year
+  checkouts_partial_years <- checkouts_all %>%
+    group_by(aar) %>%       # Create group objects for each year. filter() and summarise() will run for each
+    filter( transact_date >= as.Date( paste0(aar,"-01-01" )) & transact_date <= as.Date( paste0(aar,substr(max_date[[1]],5,11) ))) %>%
+    summarise(sum = sum(antal)) %>%
+    mutate(dato = paste0(aar,substr(max_date[[1]],5,11))) 
+  
+  # Ditto whole years
+  checkouts_whole_years <- checkouts_all %>%
+    group_by(aar) %>%
+    summarise(sum = sum(antal)) %>%
+    mutate(dato = paste0(aar,"-12-31")) 
+  
+  # Union the partial and whole years and order them by year and date (desc) 
+  checkouts_samedate <- rbind(checkouts_whole_years,checkouts_partial_years) %>%
+    arrange(aar, desc(dato))
+  
+  checkouts_samedate <- checkouts_samedate[c(1,3,2)]      # does the column order matter?
+  names(checkouts_samedate) <- c("year","date","count")   # unsure if the column names matter, but renaming just to be safe
+  
+  curDate <- format(Sys.Date()-3, format="%Y-%m-%d") # the matching date you want data from, across all the years on the x-axis
+  sortx <- "desc"         # controls direction of the sorting of the years on the x-axis
+  frontColors <- colors # this vector turns into a javascript array
+  backColor <- "Gainsboro"
+  vizWidth <- ""
+  labelx <- "År"
+  labely <- "Antal"
+  tickNumY <- 7
+  showScaleY <- "false"
+  fontSizeX <- ""
+  fontSizeY <- ""
+  barWidth <- 0.8    # This is a percentage. 1 means no gap between bars (i.e. 100%)
+  barsOffset <- 10
+  
+  output$checkouts_samedate_plot <- renderSamedate_barchart({
+    samedate_barchart(checkouts_samedate,curDate,sortx,frontColors,backColor,labelx,labely,tickNumY,showScaleY,barWidth,barsOffset)
+  })
   
   # visitors total plot#
   output$checkouts_plot_all <- renderPlotly({
