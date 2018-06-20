fab85dd1828032e16a512d988af178321333e1a4
kentgottschalk <kentgottschalk@gmail.com>
2018-06-20 15:13:10 +0200

    aktiv/inaktive tabel done

--- sections/users.R
+++ sections/users.R
@@ -48,8 +48,8 @@ usersTabPanelUI <- function(id) {
                                                   p("Lånergrafen udgøres af data fra Cicero."),
                                                   p("Borgergrafen udgøres af data fra seneste kvartal fra Danmarks Statistik."),
                                                   plotlyOutput(ns("agebranch_plot")),
+                                                  h4("Aldersfordeling pr. filial"),
                                                   p("Graferne viser aldersfordelingen blandt lånerne, fordelt efter tilhørsfilial."),
-                                                  h4("Aldersfordling pr. filial"),
                                                   branchplotUI(ns(id = "kor"), branch = "Korup"),
                                                   branchplotUI(ns(id = "ta"), branch = "Tarup"),
                                                   branchplotUI(ns(id = "da"), branch = "Dalum"),
@@ -63,12 +63,12 @@ usersTabPanelUI <- function(id) {
                                                   p("OBS. Lånere kan optræde i flere kategorier"),
                                                   tableOutput(ns('tableloaners')),
                                                   csvDownloadUI(ns("inner2"))))),
-                          tabPanel("Kort",
-                                   fluidRow(width = 12,
-                                            column(width = 12,
-                                                   leafletOutput("mymap")
-                                                   ))
-                          ),
+                          # tabPanel("Kort",
+                          #          fluidRow(width = 12,
+                          #                   column(width = 12,
+                          #                          leafletOutput("mymap")
+                          #                          ))
+                          # ),
                           tabPanel("Dokumentation og Data",
                                    fluidRow(width = 12,
                                             column(width = 12,
@@ -94,59 +94,23 @@ usersTabPanelUI <- function(id) {
 # SERVER
 
 usersTabPanel <- function(input, output, session, data, tablename) {
-  # module_data <- reactive({
-  #   data %>% filter(name == tablename)
-  # })
-  
+
   ### DB QUERIES ###
   drv <- dbDriver("PostgreSQL")
   con <- dbConnect(drv, dbname = dbname, host = host, port = port, user = user, password = password)
   
-  loaners <- dbGetQuery(con, "select case 
-    when cicero.aktive_laanere.Name like '0%'::text OR cicero.aktive_laanere.Name like '0%'::text 
-    OR cicero.aktive_laanere.Name like '1%'::text OR cicero.aktive_laanere.Name like '2%'::text 
-    OR cicero.aktive_laanere.Name like '3%'::text OR cicero.aktive_laanere.Name like '4%'::text 
-    OR cicero.aktive_laanere.Name like '5%'::text OR cicero.aktive_laanere.Name like '6%'::text 
-    OR cicero.aktive_laanere.Name like '7%'::text then 'Skole'::text
-    when cicero.aktive_laanere.Name = 'Voksen, Odense kommune' then 'Voksen, Odense kommune'
-    when cicero.aktive_laanere.Name = 'Barn, Odense kommune' then 'Barn, Odense kommune'
-    when cicero.aktive_laanere.Name = 'Voksen, udenfor Odense Kommune' then 'Voksen, udenfor Odense kommune'
-    when cicero.aktive_laanere.Name = 'Barn, udenfor Odense Kommune' then 'Barn, udenfor Odense kommune'  
-    else 'Andre' end as Kategori, 
-    sum(cicero.aktive_laanere.loaner_stat_count)::text as Aktive, 
-    (sum(cicero.inaktive_laanere.loaner_stat_count)-sum(cicero.aktive_laanere.loaner_stat_count))::text as Inaktive, 
-    (sum(cicero.inaktive_laanere.loaner_stat_count))::text as Alle
-    from cicero.aktive_laanere 
-    join cicero.inaktive_laanere on cicero.aktive_laanere.Name = cicero.inaktive_laanere.Name
-    group by kategori")
+  loaners_active <- dbGetQuery(con, "select * from cicero.aktive_laanere")
+  loaners_inactive <- dbGetQuery(con, "select * from cicero.inaktive_laanere")
   agebranch <- dbGetQuery(con, "select * from cicero.brugere_alder_filial")
   ageodense <- dbGetQuery(con, "select * from datamart.odenseborgere")
   dbDisconnect(con)
-  
-  colnames(loaners) <- c("Kategori", "Aktive", "Inaktive","Alle")
-  
-  loaners <- loaners %>%
-    mutate(
-      Aktive = format(round(as.numeric(Aktive), 0), nsmall=0, big.mark="."),
-      Inaktive = format(round(as.numeric(Inaktive), 0), nsmall=0, big.mark="."),     
-      Alle = format(round(as.numeric(Alle), 0), nsmall=0, big.mark=".")     
-    )
-  
-  output$tableloaners <- renderTable(loaners)
-  
-  innerResult <- callModule(csvDownload, "inner2", data = loaners, name = "users")
-  
-  
   
   # Alderfordeling
   agecitizenloaner <- agebranch %>%
     group_by(full_date1) %>%
     summarise(sum = sum(loaner_stat_count1)) %>%
-    full_join(ageodense, by = c("full_date1" = "alder")) #%>%
-    #rename("Alder" = full_date1, "Antal Lånere" = sum, "Antal Borgere" = antal)
-  
-  output$agebranch <- renderTable(agebranch) 
-  
+    full_join(ageodense, by = c("full_date1" = "alder"))
+
   output$agebranch_plot <- renderPlotly({
     plot_ly(agecitizenloaner, x = agecitizenloaner$full_date1, y = agecitizenloaner$sum, type = 'bar', name = 'Lånere', marker = list(color = color1)) %>%
       add_trace(y = agecitizenloaner$antal, name = 'Borgere i Odense', marker = list(color = color2) ) %>%
@@ -162,12 +126,50 @@ usersTabPanel <- function(input, output, session, data, tablename) {
   callModule(branchplot, id = "bo", data = agebranch, branch = 'Bolbro Bibliotek')
   callModule(branchplot, id = "ho", data = agebranch, branch = "Holluf Pile Bibliotek")
   
-  # kort
-    output$mymap <- renderLeaflet({
-    m <- leaflet() %>%
-      addTiles() %>%
-      setView(lng=-73.935242, lat=40.730610 , zoom=10)
-    m
-  })
+  loaners_active <- loaners_active %>%
+    mutate(
+      location = case_when(
+        loaners_active$name == "Voksen, Odense kommune" ~ "Voksen, Odense kommune",
+        loaners_active$name == "Barn, Odense kommune" ~ "Barn, Odense kommune",
+        loaners_active$name == "Voksen, udenfor Odense Kommune" ~ "Voksen, udenfor Odense Kommune",
+        loaners_active$name == "Barn, udenfor Odense Kommune" ~ "Barn, udenfor Odense Kommune",
+        grepl("^[0-9]", loaners_active$name) ~ "Skole",
+        TRUE ~ "Andet"
+      ) 
+    )
+  loaners_active <- loaners_active %>%
+    select(-name) %>% mutate(type = "Aktive")
+  
+  loaners_inactive <- loaners_inactive %>%
+    mutate(
+      location = case_when(
+        loaners_inactive$name == "Voksen, Odense kommune" ~ "Voksen, Odense kommune",
+        loaners_inactive$name == "Barn, Odense kommune" ~ "Barn, Odense kommune",
+        loaners_inactive$name == "Voksen, udenfor Odense Kommune" ~ "Voksen, udenfor Odense Kommune",
+        loaners_inactive$name == "Barn, udenfor Odense Kommune" ~ "Barn, udenfor Odense Kommune",
+        grepl("^[0-9]", loaners_inactive$name) ~ "Skole",
+        TRUE ~ "Andet"
+      ) 
+    )
+  loaners_inactive <- loaners_inactive %>%
+    select(-name) %>% mutate(type = "Inaktive")
+  
+  loaners <- rbind(loaners_active, loaners_inactive) 
+  
+  loaners <- loaners %>% 
+    group_by(lånertype = location, dato = format(date, format="%Y-%m-%d")) %>%
+    summarise(aktive = sum(ifelse(type == "Aktive", loaner_stat_count, 0)), inaktive = sum(loaner_stat_count)-aktive, total = sum(loaner_stat_count))
+  
+  output$tableloaners <- renderTable(loaners)
+  
+  innerResult <- callModule(csvDownload, "inner2", data = loaners, name = "users")
+  
+  # # kort
+  #   output$mymap <- renderLeaflet({
+  #   m <- leaflet() %>%
+  #     addTiles() %>%
+  #     setView(lng=-73.935242, lat=40.730610 , zoom=10)
+  #   m
+  # })
   
 }
