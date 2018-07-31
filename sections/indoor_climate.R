ca80b0a13ac88dd5bdcf6b5af0cd383ed0f89e35
Søren <soeb@odense.dk>
2018-07-31 16:37:01 +0200

    refactor refactor refactor indoor climate

--- sections/indoor_climate.R
+++ sections/indoor_climate.R
@@ -6,11 +6,11 @@ source("~/.postpass")
 ### Deviceplot MODUL ###
 
 # UI
-device_plotUI <- function(id,device) {
+device_plotUI <- function(id,room) {
   ns <- NS(id)
   tagList(
     column(3,
-        p(device),
+        p(room),
         plotlyOutput(ns("sensors_plot"))
     )
   )
@@ -19,6 +19,12 @@ device_plotUI <- function(id,device) {
 # SERVER
 sensors_plot <- function(input, output, session, data, device, sensor, limits, ticksuffix) {
   
+  # Af en eller anden grund skal device kaldes eksplicit her, ellers vil der kun blive outputtet
+  # grafer fra een enkelt device (den sidste) uanset at der inputtes forskellige device id'er
+  # når denne funktion kaldes længere nede. Dvs. det er ikke nok at device er tilstede i funktionens parameter-variable ovenfor.
+  # Det må være en sær scoping/caching/optimerings/reactive issue...
+  device
+
   output$sensors_plot <- renderPlotly({
     data <- data %>%
       select_("device_id","realtime","dato","hour",.dots = sensor) %>%
@@ -26,7 +32,7 @@ sensors_plot <- function(input, output, session, data, device, sensor, limits, t
       group_by(device_id,dato,hour) %>%
       summarise_at(sensor,mean) %>%
       spread_("dato",sensor) %>%
-      mutate(avg=rowMeans(.[-1:-2])) %>%
+      mutate(avg=rowMeans(.[-1:-2])) %>%    # .[-1,-2] betyder, at man tager alle columns bortset fra nr. 1 til nr. 2 (dvs. en range)
       select(avg,everything()) # move averages to first column, so we can have an open-ended/dynamic number of measured data columns for later use
     
     # Tilføj måledagenes graflinjer:
@@ -59,6 +65,17 @@ sensors <- dbGetQuery(con, "select *
                       ;")
 dbDisconnect(con)
 
+sensors_pr_device <- sensors %>%
+  select(device_id,realtime,temperature,humidity,co2,noise_avg,noise_peak) %>%
+  # Gotta input timezone since dates have datatype 'timestamp with timezone'
+  mutate(dato = as.Date(realtime,tz="Europe/Copenhagen")) %>%
+  mutate(hour = format(realtime,'%H',tz="Europe/Copenhagen"))
+
+# create small df for looping through when calling the module's function and ui
+device_ids = sensors %>% distinct(device_id,room)
+#device_ids = c("1BE1CA2D","879D5B2C","8CAE312E","20F2A02F","F358EC2B","DDBFED32")
+
+
 
 indoor_climateTabPanelUI <- function(id) {
 
@@ -82,12 +99,12 @@ tabItem(tabName = "indoor_climate",
                                       p("Den kraftige sorte linje viser temperaturgennemsnittet for de otte dage og de grå linjer er de egentlige målinger, således at maximum og minimumværdier kan aflæses"),
                                       p("De stiplede snorhøjder på 21°C og 25°C angiver det temperaturvindue, som målingerne ifølge SDU bør ligge indenfor"),
                                       tags$br(),tags$br(),tags$br(),
-                                      device_plotUI(ns(id = "temp-1BE1CA2D"),device = "1BE1CA2D"),
-                                      device_plotUI(ns(id = "temp-879D5B2C"),device = "879D5B2C"),
-                                      device_plotUI(ns(id = "temp-8CAE312E"),device = "8CAE312E"),
-                                      device_plotUI(ns(id = "temp-20F2A02F"),device = "20F2A02F"),
-                                      device_plotUI(ns(id = "temp-F358EC2B"),device = "F358EC2B"),
-                                      device_plotUI(ns(id = "temp-DDBFED32"),device = "DDBFED32")
+                                      device_plotUI(ns(id = "temp-1BE1CA2D"),room = "1BE1CA2D"),
+                                      device_plotUI(ns(id = "temp-879D5B2C"),room = "879D5B2C"),
+                                      device_plotUI(ns(id = "temp-8CAE312E"),room = "8CAE312E"),
+                                      device_plotUI(ns(id = "temp-20F2A02F"),room = "20F2A02F"),
+                                      device_plotUI(ns(id = "temp-F358EC2B"),room = "F358EC2B"),
+                                      device_plotUI(ns(id = "temp-DDBFED32"),room = "DDBFED32")
                                    )
                                  )
                         ),
@@ -100,12 +117,12 @@ tabItem(tabName = "indoor_climate",
                                       p("Den kraftige sorte linje viser temperaturgennemsnittet for de 7 dage og de grå linjer er de egentlige målinger, således at maximum og minimumværdier kan aflæses"),
                                       p("Den stiplede snorhøjde på 1.000ppm angiver den mængde CO2, som ifølge SDU maximalt bør være til stede"),
                                       tags$br(),tags$br(),tags$br(),
-                                      device_plotUI(ns(id = "co2-1BE1CA2D"),device = "1BE1CA2D"),
-                                      device_plotUI(ns(id = "co2-879D5B2C"),device = "879D5B2C"),
-                                      device_plotUI(ns(id = "co2-8CAE312E"),device = "8CAE312E"),
-                                      device_plotUI(ns(id = "co2-20F2A02F"),device = "20F2A02F"),
-                                      device_plotUI(ns(id = "co2-F358EC2B"),device = "F358EC2B"),
-                                      device_plotUI(ns(id = "co2-DDBFED32"),device = "DDBFED32")
+                                      device_plotUI(ns(id = "co2-1BE1CA2D"),room = "1BE1CA2D"),
+                                      device_plotUI(ns(id = "co2-879D5B2C"),room = "879D5B2C"),
+                                      device_plotUI(ns(id = "co2-8CAE312E"),room = "8CAE312E"),
+                                      device_plotUI(ns(id = "co2-20F2A02F"),room = "20F2A02F"),
+                                      device_plotUI(ns(id = "co2-F358EC2B"),room = "F358EC2B"),
+                                      device_plotUI(ns(id = "co2-DDBFED32"),room = "DDBFED32")
                                    )
                                  )
                         ),
@@ -116,12 +133,12 @@ tabItem(tabName = "indoor_climate",
                                       p("Nedenstående visualiseringer viser målinger af luftfugtighed fra IC-meter enheder i Borgernes Hus over de senste syv dage, fordelt over timer på dagen"),
                                       p("Den kraftige sorte linje viser gennemsnittet for de 7 dage og de grå linjer er de egentlige målinger, således at maximum og minimumværdier kan aflæses"),
                                       tags$br(),tags$br(),tags$br(),
-                                      device_plotUI(ns(id = "fugt-1BE1CA2D"),device = "1BE1CA2D"),
-                                      device_plotUI(ns(id = "fugt-879D5B2C"),device = "879D5B2C"),
-                                      device_plotUI(ns(id = "fugt-8CAE312E"),device = "8CAE312E"),
-                                      device_plotUI(ns(id = "fugt-20F2A02F"),device = "20F2A02F"),
-                                      device_plotUI(ns(id = "fugt-F358EC2B"),device = "F358EC2B"),
-                                      device_plotUI(ns(id = "fugt-DDBFED32"),device = "DDBFED32")
+                                      device_plotUI(ns(id = "fugt-1BE1CA2D"),room = "1BE1CA2D"),
+                                      device_plotUI(ns(id = "fugt-879D5B2C"),room = "879D5B2C"),
+                                      device_plotUI(ns(id = "fugt-8CAE312E"),room = "8CAE312E"),
+                                      device_plotUI(ns(id = "fugt-20F2A02F"),room = "20F2A02F"),
+                                      device_plotUI(ns(id = "fugt-F358EC2B"),room = "F358EC2B"),
+                                      device_plotUI(ns(id = "fugt-DDBFED32"),room = "DDBFED32")
                                    )
                                  )
                         ),
@@ -132,12 +149,12 @@ tabItem(tabName = "indoor_climate",
                                       p("Nedenstående visualiseringer viser målinger af gennemsnitligt lydniveau fra IC-meter enheder i Borgernes Hus over de senste syv dage, fordelt over timer på dagen"),
                                       p("Den kraftige sorte linje viser gennemsnittet for de 7 dage og de grå linjer er de egentlige målinger, således at maximum og minimumværdier kan aflæses"),
                                       tags$br(),tags$br(),tags$br(),
-                                      device_plotUI(ns(id = "lyd-1BE1CA2D"),device = "1BE1CA2D"),
-                                      device_plotUI(ns(id = "lyd-879D5B2C"),device = "879D5B2C"),
-                                      device_plotUI(ns(id = "lyd-8CAE312E"),device = "8CAE312E"),
-                                      device_plotUI(ns(id = "lyd-20F2A02F"),device = "20F2A02F"),
-                                      device_plotUI(ns(id = "lyd-F358EC2B"),device = "F358EC2B"),
-                                      device_plotUI(ns(id = "lyd-DDBFED32"),device = "DDBFED32")
+                                      device_plotUI(ns(id = "lyd-1BE1CA2D"),room = "1BE1CA2D"),
+                                      device_plotUI(ns(id = "lyd-879D5B2C"),room = "879D5B2C"),
+                                      device_plotUI(ns(id = "lyd-8CAE312E"),room = "8CAE312E"),
+                                      device_plotUI(ns(id = "lyd-20F2A02F"),room = "20F2A02F"),
+                                      device_plotUI(ns(id = "lyd-F358EC2B"),room = "F358EC2B"),
+                                      device_plotUI(ns(id = "lyd-DDBFED32"),room = "DDBFED32")
                                    )
                                  )
                         ),
@@ -153,36 +170,12 @@ tabItem(tabName = "indoor_climate",
 
 indoor_climateTabPanel <- function(input, output, session, data, tablename) {
   
-  sensors_pr_device <- sensors %>%
-    # Gotta input timezone since dates have datatype 'timestamp with timezone'
-    mutate(dato = as.Date(realtime,tz="Europe/Copenhagen")) %>%
-    mutate(hour = format(realtime,'%H',tz="Europe/Copenhagen"))
+  # Run the plot module for all devices and each of their four sensors:
+  for(row in 1:nrow(device_ids)){
+   callModule(sensors_plot, id = paste0("temp-",device_ids[row,"device_id"]), data = sensors_pr_device, device = device_ids[row,"device_id"], sensor = 'temperature', ticksuffix = '°C ', limits = c(21,25))
+   callModule(sensors_plot, id = paste0("co2-",device_ids[row,"device_id"]), data = sensors_pr_device, device = device_ids[row,"device_id"], sensor = 'co2', ticksuffix = 'ppm ', limits = 1000)
+   callModule(sensors_plot, id = paste0("fugt-",device_ids[row,"device_id"]), data = sensors_pr_device, device = device_ids[row,"device_id"], sensor = 'humidity', ticksuffix = '% ', limits = '')
+   callModule(sensors_plot, id = paste0("lyd-",device_ids[row,"device_id"]), data = sensors_pr_device, device = device_ids[row,"device_id"], sensor = 'noise_avg', ticksuffix = 'dBa ', limits = 60)
+  }
 
-  callModule(sensors_plot, id = 'temp-1BE1CA2D', data = sensors_pr_device, device = '1BE1CA2D', sensor = 'temperature', ticksuffix = '°C ', limits = c(21,25))
-  callModule(sensors_plot, id = 'temp-879D5B2C', data = sensors_pr_device, device = '879D5B2C', sensor = 'temperature', ticksuffix = '°C ', limits = c(21,25))
-  callModule(sensors_plot, id = 'temp-8CAE312E', data = sensors_pr_device, device = '8CAE312E', sensor = 'temperature', ticksuffix = '°C ', limits = c(21,25))
-  callModule(sensors_plot, id = 'temp-20F2A02F', data = sensors_pr_device, device = '20F2A02F', sensor = 'temperature', ticksuffix = '°C ', limits = c(21,25))
-  callModule(sensors_plot, id = 'temp-F358EC2B', data = sensors_pr_device, device = 'F358EC2B', sensor = 'temperature', ticksuffix = '°C ', limits = c(21,25))
-  callModule(sensors_plot, id = 'temp-DDBFED32', data = sensors_pr_device, device = 'DDBFED32', sensor = 'temperature', ticksuffix = '°C ', limits = c(21,25))
-
-  callModule(sensors_plot, id = 'co2-1BE1CA2D', data = sensors_pr_device, device = '1BE1CA2D', sensor = 'co2', ticksuffix = 'ppm ', limits = 1000)
-  callModule(sensors_plot, id = 'co2-879D5B2C', data = sensors_pr_device, device = '879D5B2C', sensor = 'co2', ticksuffix = 'ppm ', limits = 1000)
-  callModule(sensors_plot, id = 'co2-8CAE312E', data = sensors_pr_device, device = '8CAE312E', sensor = 'co2', ticksuffix = 'ppm ', limits = 1000)
-  callModule(sensors_plot, id = 'co2-20F2A02F', data = sensors_pr_device, device = '20F2A02F', sensor = 'co2', ticksuffix = 'ppm ', limits = 1000)
-  callModule(sensors_plot, id = 'co2-F358EC2B', data = sensors_pr_device, device = 'F358EC2B', sensor = 'co2', ticksuffix = 'ppm ', limits = 1000)
-  callModule(sensors_plot, id = 'co2-DDBFED32', data = sensors_pr_device, device = 'DDBFED32', sensor = 'co2', ticksuffix = 'ppm ', limits = 1000)
-  
-  callModule(sensors_plot, id = 'fugt-1BE1CA2D', data = sensors_pr_device, device = '1BE1CA2D', sensor = 'humidity', ticksuffix = '% ', limits = '')
-  callModule(sensors_plot, id = 'fugt-879D5B2C', data = sensors_pr_device, device = '879D5B2C', sensor = 'humidity', ticksuffix = '% ', limits = '')
-  callModule(sensors_plot, id = 'fugt-8CAE312E', data = sensors_pr_device, device = '8CAE312E', sensor = 'humidity', ticksuffix = '% ', limits = '')
-  callModule(sensors_plot, id = 'fugt-20F2A02F', data = sensors_pr_device, device = '20F2A02F', sensor = 'humidity', ticksuffix = '% ', limits = '')
-  callModule(sensors_plot, id = 'fugt-F358EC2B', data = sensors_pr_device, device = 'F358EC2B', sensor = 'humidity', ticksuffix = '% ', limits = '')
-  callModule(sensors_plot, id = 'fugt-DDBFED32', data = sensors_pr_device, device = 'DDBFED32', sensor = 'humidity', ticksuffix = '% ', limits = '')
-  
-  callModule(sensors_plot, id = 'lyd-1BE1CA2D', data = sensors_pr_device, device = '1BE1CA2D', sensor = 'noise_avg', ticksuffix = 'dBa ', limits = 60)
-  callModule(sensors_plot, id = 'lyd-879D5B2C', data = sensors_pr_device, device = '879D5B2C', sensor = 'noise_avg', ticksuffix = 'dBa ', limits = 60)
-  callModule(sensors_plot, id = 'lyd-8CAE312E', data = sensors_pr_device, device = '8CAE312E', sensor = 'noise_avg', ticksuffix = 'dBa ', limits = 60)
-  callModule(sensors_plot, id = 'lyd-20F2A02F', data = sensors_pr_device, device = '20F2A02F', sensor = 'noise_avg', ticksuffix = 'dBa ', limits = 60)
-  callModule(sensors_plot, id = 'lyd-F358EC2B', data = sensors_pr_device, device = 'F358EC2B', sensor = 'noise_avg', ticksuffix = 'dBa ', limits = 60)
-  callModule(sensors_plot, id = 'lyd-DDBFED32', data = sensors_pr_device, device = 'DDBFED32', sensor = 'noise_avg', ticksuffix = 'dBa ', limits = 60)
 }
