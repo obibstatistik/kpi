82ba31b67f8aaa6e5256607cc4dc88ed80e49bc6
sorenb <soeb@odense.dk>
2018-10-18 07:59:04 +0200

    working on using stylesheet instead of js in print page styling

--- www/hacks.js
+++ www/hacks.js
@@ -17,25 +17,36 @@ function printDiv(event,parentClass) {
   widgetDivs.css('width','700px');          
   widgetDivs.each(function() { autorangeChart(this.id); });                                                       // force svgs to the parent divs' width
   w = window.open();                                                                                              // open a new window/html document
+  
+  var sc = w.document.createElement("link");
+  sc.setAttribute("rel", "stylesheet");
+  sc.setAttribute("href", "plotprint.css");
+  sc.setAttribute("type", "text/css");
+  w.document.head.appendChild(sc);
+  
   w.document.write($(divName).html());                                                                            // write the saved html to the new empty window
   var svgs = w.document.getElementsByClassName("main-svg");                                                       // get refs to all elements with class main-svg
   [].forEach.call(svgs, function (svg) {svg.setAttribute('style','position:absolute;')});                         // set position absolute for all svgs to make axis label svg and graph svg stay on top of each other
-  var a = w.document.getElementsByTagName("a");                                                                   // get refs to all a elements (e.g. the print button)
-  while (a[0]) a[0].parentNode.removeChild(a[0]);                                                                 // remove all a elements
-  var inputs = w.document.getElementsByTagName("input");                                                          // get refs to all input elements (e.g. the print button)
-  while (inputs[0]) inputs[0].parentNode.removeChild(inputs[0]);                                                  // remove all input elements
-  var modebars = w.document.getElementsByClassName("modebar");                                                    // get refs to modebars i.e. the plotly toolbar
-  while (modebars[0]) modebars[0].parentNode.removeChild(modebars[0]);                                            // remove all modebar elements ()
-  var controlLabels = w.document.getElementsByClassName("control-label");                                         // get refs to modebars i.e. the plotly toolbar
-  [].forEach.call(controlLabels, function (controlLabel) {controlLabel.setAttribute('style','float:left; margin: 0 20px 0 0')}); 
-  var tables = w.document.getElementsByTagName("table");                                                          // get refs to modebars i.e. the plotly toolbar
-  [].forEach.call(tables, function (table) {table.setAttribute('style','width: 700px; margin: 40px 0 0 0; border-spacing: 0;')});
-  var tds = w.document.getElementsByTagName("td");                                                                // get refs to modebars i.e. the plotly toolbar
-  [].forEach.call(tds, function (td) {td.setAttribute('style','border-top: solid 1px lightgrey; text-align: right;')});
-  //var tdCol1 = w.document.getElementsByTagName("td");                                                                // get refs to modebars i.e. the plotly toolbar
+  //var a = w.document.getElementsByTagName("a");                                                                   // get refs to all a elements (e.g. the print button)
+  //while (a[0]) a[0].parentNode.removeChild(a[0]);                                                                 // remove all a elements
+  //var inputs = w.document.getElementsByTagName("input");                                                          // get refs to all input elements (e.g. the print button)
+  //while (inputs[0]) inputs[0].parentNode.removeChild(inputs[0]);                                                  // remove all input elements
+  //var modebars = w.document.getElementsByClassName("modebar");                                                    // get refs to modebars i.e. the plotly toolbar
+  //while (modebars[0]) modebars[0].parentNode.removeChild(modebars[0]);                                            // remove all modebar elements ()
+  //var controlLabels = w.document.getElementsByClassName("control-label");                                         // get refs to modebars i.e. the plotly toolbar
+  //[].forEach.call(controlLabels, function (controlLabel) {controlLabel.setAttribute('style','float:left; margin: 0 20px 0 0')}); 
+  //var tables = w.document.getElementsByTagName("table");                                                          // get refs to modebars i.e. the plotly toolbar
+  //[].forEach.call(tables, function (table) {table.setAttribute('style','width: 700px; margin: 40px 0 0 0; border-spacing: 0;')});
+  //var tds = w.document.getElementsByTagName("td");                                                                // get refs to modebars i.e. the plotly toolbar
   //[].forEach.call(tds, function (td) {td.setAttribute('style','border-top: solid 1px lightgrey; text-align: right;')});
+  
+  //var tdCol1 = w.document.getElementsByTagName("td");                                                           // get refs to modebars i.e. the plotly toolbar
+  //[].forEach.call(tds, function (td) {td.setAttribute('style','border-top: solid 1px lightgrey; text-align: right;')});
+  
+  //w.location.reload(false);
+  
   w.print();                                                                                                      // open the print dialog
-  w.close();                                                                                                    // close the new window
+  //w.close();                                                                                                      // close the new window
   widgetDivs.css('width','100%');                                                                                 // reset widths of plot divs and the like
   widgetDivs.each(function() { autorangeChart(this.id); });                                                       // force svgs back to original widths
 }
