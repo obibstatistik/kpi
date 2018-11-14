b638fa2c432416ddfa20253962f65a33b9eebb39
sorenb <soeb@odense.dk>
2018-11-14 22:59:48 +0100

    improved stats for forfatterweb and faktalink

--- sections/edatabases.R
+++ sections/edatabases.R
@@ -16,7 +16,7 @@ edatabasesTabPanelUI <- function(id) {
   
   tabItem(tabName = "edatabases",
           box(width = 12, solidHeader = TRUE, id = "edatabasesheader",
-              h3("Elektroniske Ressourcer"),
+              h3("eBaser"),
               img(src='icons/eressourcer_negativ_45x45.png', align = "right", height="46px")
           ),     
           fluidRow(
@@ -30,17 +30,18 @@ edatabasesTabPanelUI <- function(id) {
                                                    selectInput(ns("eres_vendor"),"eRessource:", unique(dbc_eres_stats$vendor)),
                                                    selectInput(ns("eres_stattype"),"Statistik:", unique(dbc_eres_stats$stattype)),
                                                    selectInput(ns("eres_aar"),"År:", unique(dbc_eres_stats$aar)),
-                                                   checkboxGroupInput(ns("isil_selector"),
+                                                   p("Vælg evt. andre CB'er eller København og Aarhus til sammenligning:"),
+                                                   checkboxGroupInput(ns("eres_isil"),
                                                                       'Vælg biblioteker:',
-                                                                      unique(as.character(dbc_eres_stats$isil)),
-                                                                      selected = unique(as.character(dbc_eres_stats$isil)),
-                                                                      inline = F)#,
-                                                   #xlsxDownloadUI(ns("inventory")),
-                                                   #tags$div(HTML('<a id="print-checkouts" class="btn btn-default btn-print" onclick="printDiv.call(this,event,\'.col-sm-12\',\'700px\')"><i class="fa fa-print"></i> Print denne sektion</a>'))
+                                                                      unique(as.character(isil2name(dbc_eres_stats$isil))),
+                                                                      selected = unique(as.character(isil2name(dbc_eres_stats$isil))),
+                                                                      inline = F),
+                                                   xlsxDownloadUI(ns("edatabases")),
+                                                   tags$div(HTML('<a id="print-checkouts" class="btn btn-default btn-print" onclick="printDiv.call(this,event,\'.col-sm-12\',\'700px\')"><i class="fa fa-print"></i> Print denne sektion</a>'))
                                             ),
                                             column(10,
-                                                   h4("eRessource statistik fra DBC"),
-                                                   p("Følgende statistik stammer fra https://bibstats.dbc.dk"),
+                                                   h4("Faktalink og Forfatterweb"),
+                                                   span("Følgende statistik stammer fra"),a("https://bibstats.dbc.dk", href = "https://bibstats.dbc.dk"),
                                                    plotlyOutput(ns("dbc_eres_stats_plot"))#,
                                                    #formattableOutput(ns("dbc_eres_stats_table"))
                                             )
@@ -52,47 +53,48 @@ edatabasesTabPanelUI <- function(id) {
 edatabasesTabPanel <- function(input, output, session, data, tablename) {
   
   # Manuel, kronologisk sortering af data fra ud fra måneder, så det ikke bliver alfabetisk
-  #licenses_df$month <- factor(licenses_df$month, levels = c("jan","feb","mar","apr","maj","jun","jul","aug","sep","okt","nov","dec"))
-  
-  # Call Excel download function for tables 
-  #callModule(xlsxDownload, "edatabases", data = reactive(checkouts_all_tbl()), name = "eBaser")
+  #dbc_eres_stats$maaned <- factor(dbc_eres_stats$maaned, levels = c("januar","februar","marts","april","maj","juni","juli","august","september","oktober","november","december"))
   
   dbc_eres_stats_df <- reactive({
     dbc_eres_stats <- dbc_eres_stats %>%
       mutate_at(vars(2), funs(isil2name(.))) %>%
-      filter(vendor == 'Forfatterweb') %>%
-      filter(stattype == 'visits') %>%
-      filter(aar == '2018') %>%
+      filter(vendor == input$eres_vendor) %>%
+      filter(stattype == input$eres_stattype) %>%
+      filter(aar == input$eres_aar) %>%
+      filter(isil %in% input$eres_isil) %>%
       select(isil,vendor,stattype,aar,maaned,antal) %>%
       mutate_at(vars(5), funs(danskemåneder(.))) %>%
       group_by(maaned, isil) %>%
       spread(key = isil, value = antal)
   })
   
-  # Render the plot
-  output$dbc_eres_stats_plot <- renderPlotly({   
-    plot_ly(dbc_eres_stats_df(), x = ~maaned, y = ~Odense, type = 'bar', name = 'Odense', marker = list(color = color1)) %>%
-      add_trace(y = ~Aalborg, name = 'Aalborg', marker = list(color = color3)) %>%
-      add_trace(y = ~Herning, name = 'Herning', marker = list(color = color2)) %>%
-      add_trace(y = ~Vejle, name = 'Vejle', marker = list(color = color4)) %>%
-      add_trace(y = ~Aarhus, name = 'Aarhus', marker = list(color = color5)) %>%
-      add_trace(y = ~København, name = 'København', marker = list(color = color6)) %>%
-      add_trace(y = ~Roskilde, name = 'Roskilde', marker = list(color = color7)) %>%
-      add_trace(y = ~Gentofte, name = 'Gentofte', marker = list(color = color8)) %>%
-      layout(autosize = TRUE, yaxis = list(title = 'Antal'), xaxis = list(title = 'Måned', dtick = 1, autotick = FALSE), barmode = 'group')
-  })
+  # Call Excel download function for tables 
+  callModule(xlsxDownload, "edatabases", data = reactive(dbc_eres_stats_df()), name = "ebaser")
   
-  #  # Render the plot
-  #  output$licenses_plot <- renderPlotly({
-  #    data <- lic_data() %>% spread(produkt, visninger)   # the plot needs a spread (pivot) of produkt
-  #    colNames <- names(data)[-1]                         # ie. get all colnames except the first which is year or month or whatever
-  #    # cf. https://stackoverflow.com/questions/46583282/r-plotly-to-add-traces-conditionally-based-on-available-columns-in-dataframe                                
-  #    p <- plot_ly(data, x = ~month, type = 'scatter', mode = 'lines') 
-  #    for(trace in colNames){
-  #      p <- p %>% add_trace(y = as.formula(paste0("~`", trace, "`")), name = trace, mode = 'lines')   # add_trace(y = as.formula(paste0("~`", trace, "`")), name = trace)
-  #    }
-  #    p %>% layout(xaxis = list(title = 'Måneder'), yaxis = list (title = 'Visninger'))
-  #  })
+  # Render the plot
+  #output$dbc_eres_stats_plot <- renderPlotly({   
+  #  plot_ly(dbc_eres_stats_df(), x = ~maaned, y = ~Odense, type = 'bar', name = 'Odense', marker = list(color = color1)) %>%
+  #    add_trace(y = ~Aalborg, name = 'Aalborg', marker = list(color = color3)) %>%
+  #    add_trace(y = ~Herning, name = 'Herning', marker = list(color = color2)) %>%
+  #    add_trace(y = ~Vejle, name = 'Vejle', marker = list(color = color4)) %>%
+  #    add_trace(y = ~Aarhus, name = 'Aarhus', marker = list(color = color5)) %>%
+  #    add_trace(y = ~København, name = 'København', marker = list(color = color6)) %>%
+  #    add_trace(y = ~Roskilde, name = 'Roskilde', marker = list(color = color7)) %>%
+  #    add_trace(y = ~Gentofte, name = 'Gentofte', marker = list(color = color8)) %>%
+  #    layout(autosize = TRUE, yaxis = list(title = 'Antal'), xaxis = list(title = 'Måned', dtick = 1, autotick = FALSE), barmode = 'group')
+  #})
+  
+  # Render the plot
+  output$dbc_eres_stats_plot <- renderPlotly({
+    colNames <- names(dbc_eres_stats_df())[-1:-5]                         # ie. get all colnames except the first thru the fifth
+    p <- plot_ly(dbc_eres_stats_df(), x = ~maaned, y = as.formula(paste0("~`", names(dbc_eres_stats_df())[5],"`")), type = 'bar', name = names(dbc_eres_stats_df())[5], marker = list(color = color1)) 
+    len <- length(colNames)
+    for(i in 0:len){
+      trace <- colNames[i+1]
+      p <- p %>% add_trace(y = as.formula(paste0("~", trace)), type = 'bar', name = trace, marker = list(color = colors[i+2]))
+    }
+    p %>% layout(autosize = TRUE, yaxis = list(title = 'Antal'), xaxis = list(title = 'Måned', dtick = 1, autotick = FALSE), barmode = 'group')
+  })
   
   # beholdning_alt_tbl <- reactive({
   #   beholdning_alt %>%
--- sections/materials.R
+++ sections/materials.R
@@ -92,27 +92,6 @@ materialsTabPanelUI <- function(id) {
                                             )
                                      ),
                                      column(12,tags$div( tags$hr(), class = "hidden-print" ))                                   
-                                     #column(12,
-                                     #      column(2,
-                                     #             selectInput(ns("vendor"),"eRessource:", unique(dbc_eres_stats$vendor)),
-                                     #             selectInput(ns("stattype"),"Statistik:", unique(dbc_eres_stats$stattype)),
-                                     #             selectInput(ns("aar"),"År:", unique(dbc_eres_stats$aar)),
-                                     #             checkboxGroupInput(ns("isil_selector"),
-                                     #                                'Vælg biblioteker:',
-                                     #                                unique(as.character(dbc_eres_stats$isil)),
-                                     #                                selected = unique(as.character(dbc_eres_stats$isil)),
-                                     #                                inline = F)#,
-                                     #             #xlsxDownloadUI(ns("inventory")),
-                                     #             #tags$div(HTML('<a id="print-checkouts" class="btn btn-default btn-print" onclick="printDiv.call(this,event,\'.col-sm-12\',\'700px\')"><i class="fa fa-print"></i> Print denne sektion</a>'))
-                                     #      ),
-                                     #      column(10,
-                                     #             h4("eRessource statistik fra DBC"),
-                                     #             p("Følgende statistik stammer fra https://bibstats.dbc.dk"),
-                                     #             plotlyOutput(ns("dbc_eres_stats_plot")),
-                                     #             formattableOutput(ns("dbc_eres_stats_table"))
-                                     #      )
-                                     #)
-                                    
                                 )
                           ),
                        #   tabPanel("Timer", 
@@ -254,54 +233,7 @@ materialsTabPanel <- function(input, output, session, data, tablename) {
   
   # Call Excel download function for tables 
   callModule(xlsxDownload, "checkouts", data = reactive(checkouts_all_tbl()), name = "Udlån_på_OBB")
-  
- #dbc_eres_stats_df <- reactive({
- #  dbc_eres_stats %>%
- #    mutate_at(vars(2), funs(isil2name(.))) %>%
- #    filter(vendor == 'Forfatterweb') %>%
- #    filter(stattype == 'visits') %>%
- #    filter(aar == '2018') %>%
- #    select(isil,vendor,stattype,aar,maaned,antal) %>%
- #    mutate_at(vars(5), funs(danskemåneder(.))) %>%
- #    group_by(maaned, isil) %>%
- #    spread(key = isil, value = antal)
- #})
 
- ## Render the plot
- #output$dbc_eres_stats_plot <- renderPlotly({   
- #  plot_ly(dbc_eres_stats_df(), x = ~maaned, y = ~Odense, type = 'bar', name = 'Odense', marker = list(color = color1)) %>%
- #    add_trace(y = ~Aalborg, name = 'Aalborg', marker = list(color = color3)) %>%
- #    add_trace(y = ~Herning, name = 'Herning', marker = list(color = color2)) %>%
- #    add_trace(y = ~Vejle, name = 'Vejle', marker = list(color = color4)) %>%
- #    add_trace(y = ~Aarhus, name = 'Aarhus', marker = list(color = color5)) %>%
- #    add_trace(y = ~København, name = 'København', marker = list(color = color6)) %>%
- #    add_trace(y = ~Roskilde, name = 'Roskilde', marker = list(color = color7)) %>%
- #    add_trace(y = ~Gentofte, name = 'Gentofte', marker = list(color = color8)) %>%
- #    layout(autosize = TRUE, yaxis = list(title = 'Antal'), xaxis = list(title = 'Måned', dtick = 1, autotick = FALSE), barmode = 'group')
- #})
-    
-  #  # Render the plot
-  #  output$licenses_plot <- renderPlotly({
-  #    data <- lic_data() %>% spread(produkt, visninger)   # the plot needs a spread (pivot) of produkt
-  #    colNames <- names(data)[-1]                         # ie. get all colnames except the first which is year or month or whatever
-  #    # cf. https://stackoverflow.com/questions/46583282/r-plotly-to-add-traces-conditionally-based-on-available-columns-in-dataframe                                
-  #    p <- plot_ly(data, x = ~month, type = 'scatter', mode = 'lines') 
-  #    for(trace in colNames){
-  #      p <- p %>% add_trace(y = as.formula(paste0("~`", trace, "`")), name = trace, mode = 'lines')   # add_trace(y = as.formula(paste0("~`", trace, "`")), name = trace)
-  #    }
-  #    p %>% layout(xaxis = list(title = 'Måneder'), yaxis = list (title = 'Visninger'))
-  #  })
-    
-  # beholdning_alt_tbl <- reactive({
-  #   beholdning_alt %>%
-  #     group_by_at(vars(bibliotek,input$niveau)) %>%
-  #     summarise(antal = sum(antal)) %>%
-  #     spread(key = bibliotek, value = antal, fill = 0) %>%
-  #     select(c(input$niveau,input$branch_selector)) %>%
-  #     adorn_totals(c("row","col"))
-  # })
-    
-  
   #udlaan_heat <- udlaan %>%
   #  mutate(branch = ifelse(is.na(name), "Andet", name)) %>%
   #  group_by(branch, hour) %>%
