187dbd6b52756be7d2afd9cc74e0257ec443d217
Søren <soeb@odense.dk>
2018-12-03 15:47:59 +0100

    trying to incorporate the erms data into eressources

--- sections/eressources.R
+++ sections/eressources.R
@@ -2,23 +2,14 @@ source("global.R")
 source("modules.R")
 source("~/.postpass")
 
-#rm(list = ls())
-library(shiny)
-library(ggplot2)
-library(dplyr)
-bcl <- read.csv("http://pub.data.gov.bc.ca/datasets/176284/BC_Liquor_Store_Product_Price_List.csv", stringsAsFactors = F)
-
-
-
 ### DB QUERIES ###
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, dbname = dbname, host = host, port = port, user = user, password = password)
 licenses_df <- dbGetQuery(con, "select brug,pris,statbank,year,month,datamart.eressourcer_ddb.product produkt,sum(use) visninger 
-from datamart.eressourcer_ddb 
-left join datamart.eressourcer_ddb_kategorier on datamart.eressourcer_ddb_kategorier.name_match = datamart.eressourcer_ddb.product 
-where type = 'visninger' 
-group by brug,pris,statbank,year,month,datamart.eressourcer_ddb.product")
-#produkter <- dbGetQuery(con, "select distinct product,statbank from datamart.eressourcer_ddb_kategorier")
+                          from datamart.eressourcer_ddb 
+                          left join datamart.eressourcer_ddb_kategorier on datamart.eressourcer_ddb_kategorier.name_match = datamart.eressourcer_ddb.product 
+                          where type = 'visninger' 
+                          group by brug,pris,statbank,year,month,datamart.eressourcer_ddb.product")
 dbDisconnect(con)
 #licenses_df <- as.data.frame(licenses_df,stringsAsFactors = FALSE)
 
@@ -29,7 +20,7 @@ licensesTabPanelUI <- function(id) {
   
   tabItem(tabName = "licenses",
           box(width = 12, solidHeader = TRUE, id = "eressourcesheader",
-              h3("Licenser"),
+              h3("Elektroniske Ressourcer"),
               img(src='icons/eressourcer_negativ_45x45.png', align = "right", height="46px")
           ),     
           fluidRow(
@@ -40,35 +31,36 @@ licensesTabPanelUI <- function(id) {
                                    fluidRow(
                                      column(12,
                                             column(2,
-                                                   h4("Afgrænsning"),
-                                                   selectInput(ns("lic_fromyear_input"), "År:", unique(as.numeric(licenses_df$year))),
+                                                   h4("Afgræns"),
+                                                   selectInput(ns("lic_fromyear"), "År:", unique(as.numeric(licenses_df$year))),
                                                    #selectInput(ns("lic_statbank"), "Statistikbankens typer:", c("Seriepublikationer" = "serie","eBøger" = "ebooks","Multimedier" = "multimedia","Databaser" = "databaser")),
-                                                   #selectInput(ns("lic_priskategori"), "Priskategori:", unique(as.character(licenses_df$pris))),
-                                                   #selectInput(ns("lic_brugskategori"), "Brugskategori:", unique(as.character(licenses_df$brug)))
-                                                   #selectInput(ns("lic_statbank"), "Statistikbankens typer:", unique(as.character(licenses_df$statbank))),
-                                                   #selectInput(ns("lic_statbank_input"), "Statistikbankens typer:",sort(unique(bcl$Country))),
-                                                   selectInput(ns("countryInput"), "Country",sort(unique(bcl$PRODUCT_COUNTRY_ORIGIN_NAME))),
-                                                   sliderInput(ns("priceInput"), "Price", min = 0, max = 100, value=c(0,50), pre="$"),
-                                                   uiOutput(ns("lic_priskategori_output")),
-                                                   uiOutput(ns("lic_brugskategori_output"))
+                                                   selectInput(ns("lic_statbank"), "Statistikbankens typer:", unique(as.character(licenses_df$statbank))),
+                                                   selectInput(ns("lic_priskategori"), "Priskategori:", unique(as.character(licenses_df$pris))),
+                                                   selectInput(ns("lic_brugskategori"), "Brugskategori:", unique(as.character(licenses_df$brug))),
+                                                   radioButtons(ns("viztype"), "Graftype:", c("Linjer" = "lines", "Søjler" = "bar") )
                                             ),
                                             column(10,
                                                    h4("Licenser"),
                                                    p("Kategoriseret efter sammenlignelighed"),
-                                                   #plotlyOutput(ns("licenses_plot")),
-                                                   plotOutput(ns("coolplot")),
-                                                   tableOutput(ns("results"))
+                                                   # Only show this panel if 'lines', i.e. 'scatterplot' is selected
+                                                   conditionalPanel(
+                                                     paste0("input['", ns("viztype"), "'] == 'lines'"), plotlyOutput(ns("lic_scatterplot"))
+                                                   ),
+                                                   # Only show this panel if 'bar', i.e. 'barplot' is selected
+                                                   conditionalPanel(
+                                                     paste0("input['", ns("viztype"), "'] == 'bar'"), plotlyOutput(ns("lic_barplot"))
+                                                   )
                                             ),
                                             column(2,
-                                                   uiOutput(ns("lic_produkt_output"))
-                                                   #checkboxGroupInput(ns("lic_productselector"),
-                                                   #                      'Vælg eRessource:',
-                                                   #                      selected = unique(as.character(licenses_df$produkt)),
-                                                   #                      inline = F),s
+                                                   checkboxGroupInput(ns("lic_productselector"),
+                                                                      'Vælg eRessource:',
+                                                                      #unique(as.character(licenses_df$produkt)),
+                                                                      unique(as.character(licenses_df$produkt)),
+                                                                      selected = unique(as.character(licenses_df$produkt)),
+                                                                      inline = F)
                                             ),
                                             column(8,
-                                                   formattableOutput(ns("licenses_table")),
-                                                   tableOutput(ns('lick'))
+                                                   formattableOutput(ns("licenses_table"))
                                             ),
                                             column(12,tags$hr())
                                      )))))))
@@ -77,40 +69,27 @@ licensesTabPanelUI <- function(id) {
 # SERVER
 licensesTabPanel <- function(input, output, session, data, tablename) {
   
-  # In the server function you can get the ns from the session var
-  # this is needed for renderUI having 
-  ns <- session$ns
-  
   # Manuel, kronologisk sortering af data fra ud fra måneder, så det ikke bliver alfabetisk
   licenses_df$month <- factor(licenses_df$month, levels = c("jan","feb","mar","apr","maj","jun","jul","aug","sep","okt","nov","dec"))
   licenses_df = licenses_df[order(licenses_df$month,decreasing=FALSE),]
-
+  
+  # Store data and its filters in reactive function (gives reusability)
   lic_data <- reactive({
     licenses <- licenses_df %>%
-      #filter(year == input$lic_fromyear) %>%
-      #filter(pris == input$lic_priskategori) %>%
-      #filter(brug == input$lic_brugskategori) %>%
-      #filter(statbank == input$lic_statbank) %>%
-      
-      filter(year == input$lic_fromyear_input) %>%  # HUSK AT FILTRERE PÅ DENNE NEDENFOR!!!
-      filter(statbank == input$lic_statbank_input) %>%
-      filter(pris == input$lic_priskategori_input) %>%
-      filter(brug == input$lic_brugskategori_input) %>%
-      filter(produkt %in% input$lic_produkt_input) %>%
-      
-      select(brug,statbank,produkt,month,visninger) %>%
-      #group_by(produkt,month) %>%
-      group_by(brug,statbank,produkt,month) %>%
+      filter(year == input$lic_fromyear) %>%
+      filter(pris == input$lic_priskategori) %>%
+      filter(brug == input$lic_brugskategori) %>%
+      filter(statbank == input$lic_statbank) %>%
+      select(produkt,month,visninger) %>%
+      # filter(produkt %in% input$lic_productselector) %>%
+      group_by(produkt,month) %>%
       summarise(visninger = sum(visninger)) %>%
-      ungroup() %>%
       mutate_at(vars(-1), funs(replace(., is.na(.), 0)))
   })
   
-  # Render the plot
-  output$licenses_plot <- renderPlotly({
-    data <- lic_data() %>%
-    filter(produkt %in% input$inCheckboxGroup2) %>%
-    spread(produkt, visninger)   # the plot needs a spread (pivot) of produkt
+  # Render the plot as a scatterplot
+  output$lic_scatterplot <- renderPlotly({
+    data <- lic_data() %>% spread(produkt, visninger)   # the plot needs a spread (pivot) of produkt
     colNames <- names(data)[-1]                         # ie. get all colnames except the first which is year or month or whatever
     # cf. https://stackoverflow.com/questions/46583282/r-plotly-to-add-traces-conditionally-based-on-available-columns-in-dataframe                                
     p <- plot_ly(data, x = ~month, type = 'scatter', mode = 'lines') 
@@ -119,117 +98,21 @@ licensesTabPanel <- function(input, output, session, data, tablename) {
     }
     p %>% layout(xaxis = list(title = 'Måneder'), yaxis = list (title = 'Visninger'))
   })
-
   
-    # create a reactive to filter the dataset
-    
-    df0 <- eventReactive(input$countryInput,{
-      bcl %>% filter(PRODUCT_COUNTRY_ORIGIN_NAME %in% input$countryInput)
-    })
-    output$lic_priskategori_output <- renderUI({
-      selectInput(ns("typeInput"), "Product type",sort(unique(df0()$PRODUCT_CLASS_NAME)))
-    })
-    
-    df1 <- eventReactive(input$typeInput,{
-      print(df0())
-      df0() %>% filter(PRODUCT_CLASS_NAME %in% input$typeInput)
-    })
-    output$lic_brugskategori_output <- renderUI({
-      selectInput(ns("subtypeInput"), "Product subtype",sort(unique(df1()$PRODUCT_MINOR_CLASS_NAME)))
-    })
-    
-    df2 <- reactive({
-      print(df1())
-      df1() %>% filter(CURRENT_DISPLAY_PRICE >= input$priceInput[1],
-                       CURRENT_DISPLAY_PRICE <= input$priceInput[2],
-                       PRODUCT_MINOR_CLASS_NAME %in% input$subtypeInput)
-    })
-    
-    output$coolplot <- renderPlot({
-      ggplot(df2(), aes(PRODUCT_ALCOHOL_PERCENT)) + geom_histogram(binwidth = 1)
-    })
-    output$results <- renderTable({
-      df2()
-    })
+  # Render the plot as a barchart
+  output$lic_barplot <- renderPlotly({
+    data <- lic_data() %>% spread(produkt, visninger) 
+    colNames <- names(data)[-1]
+    p <- plot_ly(data, x = ~month, type = 'bar') 
+    for(trace in colNames){
+      p <- p %>% add_trace(y = as.formula(paste0("~`", trace, "`")), name = trace, mode = 'bar')   
+    }
+    p %>% layout(xaxis = list(title = 'Måneder'), yaxis = list (title = 'Visninger'))
+  })
   
-  
-  
- #df0 <- eventReactive(input$lic_statbank_input,{
- #  bcl %>% 
- #    rename(statbank = Country) %>%
- #    filter(statbank %in% input$lic_statbank_input)
- #})
- #output$lic_priskategori_output <- renderUI({
- #  selectInput(ns("lic_priskategori_input"), "Priskategori",sort(unique(df0()$Type)))
- #})
- #
- #df1 <- eventReactive(input$lic_priskategori_input,{
- #  print(df0())
- #  df0() %>%
- #    filter(Type %in% input$lic_priskategori_input)
- #})
- #output$lic_brugskategori_output <- renderUI({
- #  selectInput(ns("lic_brugskategori_input"), "Brugskategori",sort(unique(df1()$Subtype)))
- #})
- #
- #df2 <- reactive({
- #  print(df1())
- #  #df1() %>% filter(Price >= input$priceInput[1], Price <= input$priceInput[2],Subtype %in% input$subtypeInput)
- #  df1() %>%
- #    filter(Subtype %in% input$lic_brugskategori_input)
- #})
- #output$lic_produkt_output <- renderUI({
- #  checkboxGroupInput(ns("lic_produkt_input"), "Licens",sort(unique(lic_data()$produkt)))
- #})
- #
- #output$coolplot <- renderPlot({
- #  ggplot(df2(), aes(Alcohol_Content)) + geom_histogram(binwidth = 1)
- #})
- #output$results <- renderTable({
- #  print(df2())
- #  df2()
- #})
-
-  #output$lick <- renderTable({
-  #  lic_data()
-  #})
-  
-  
-#  observe({
-#    #x <- unique(lic_data()$statbank)
-#    y <- unique(lic_data()$produkt)
-#    z <- unique(lic_data()$pris)
-#    w <- unique(lic_data()$brug)
-#    
-#    # Can use character(0) to remove all choices
-#    #  if (is.null(z))
-#    #    z <- character(0)
-#    
-#    #updateSelectInput(session, "lic_statbank",
-#    #                  choices = x,
-#    #                  selected = x
-#    #)
-#    
-#    # Can also set the label and select items
-#    updateCheckboxGroupInput(session, "inCheckboxGroup2",
-#                             #label = paste("Checkboxgroup label", length(x)),
-#                             choices = y,
-#                             selected = y
-#    )
-#
-#    updateSelectInput(session, "lic_priskategori",
-#                      choices = z,
-#                      selected = z
-#    )   
-#    
-#    updateSelectInput(session, "lic_brugskategori",
-#                      choices = w,
-#                      selected = w
-#    )   
-#    
-# })
-    
-  #lic_loc <- reactive({
-  #  produkter <- produkter %>% filter(statbank == input$lic_statter)
-  #})
+  # Render the table
+  output$licenses_table <- renderFormattable({
+    data <- lic_data() %>% spread(month, visninger)     # the table needs a spread (pivot) of month
+    formattable(data)
+  })
 }
