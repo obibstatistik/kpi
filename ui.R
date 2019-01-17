314d839c435c6611f3c0cae827ff3f5441131512
kentgottschalk <kentgottschalk@gmail.com>
2019-01-17 14:57:48 +0100

    font awesome head link test

--- ui.R
+++ ui.R
@@ -76,6 +76,7 @@ dashboardPage(
   dashboardBody(
     tags$head(
       tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
+      tags$link(rel = "stylesheet", type = "text/css", href = "https://use.fontawesome.com/releases/v5.6.3/css/all.css", integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/", crossorigin="anonymous", media="print"),
       #tags$link(rel = "stylesheet", type = "text/css", href = "plotprint.css", media="print"),
       tags$head(tags$style(HTML('.shiny-server-account { display: none; }'))),
       #includeCSS("www/plotprint.css"),
