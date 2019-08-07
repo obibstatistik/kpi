db6a9fa288fa506de379784367bb6f1600804c17
Søren <soeb@odense.dk>
2019-08-07 13:15:20 +0200

    made some headway in the mainstream eressources department

--- sections/edatabases.R
+++ sections/edatabases.R
@@ -2,6 +2,65 @@
 drv <- dbDriver("PostgreSQL")
 con_dwh <- dbConnect(drv, dbname = dbname_dwh, host = host_dwh, port = port_dwh, user = user_dwh, password = password_dwh)
 #tablecomment <- dbGetQuery(con, paste0("select obj_description('",schema,".", table, "'::regclass)"))
+#(select regexp_matches(obj_description('eressourcer.x_filmstriben_forbrug'::regclass),'[0-9]{4}-[0-9]{2}-[0-9]{2}','g'))[1]::date rapport_dato
+
+mainstream_eres <- dbGetQuery(con_dwh, "
+SELECT 
+				dato::date laan_dato,
+		    count(*) antal,
+		    sum(pris) omkostninger,
+		    'film' mattype
+FROM eressourcer.x_filmstriben_forbrug
+WHERE extract(year from dato) >= 2012
+GROUP BY laan_dato
+UNION ALL
+SELECT 
+				oprettet::date laan_dato,
+		    count(*) antal,
+		    sum(pris) omkostninger,
+		    bogtype mattype
+FROM eressourcer.x_pubhub_udlaan
+WHERE extract(year from oprettet) >= 2012
+GROUP BY laan_dato,mattype
+")
+
+# mainstream_loan_phys_vs_eres_df <- dbGetQuery(con_dwh, "
+# SELECT i.uge,fysiske,filmstriben
+# FROM
+# 	(SELECT to_char(dato, 'YYYY - uge IW') uge, sum(antal) fysiske FROM cicero.udlaan
+# 	WHERE gmb = 'm' AND smb='th'
+# GROUP BY uge) y
+# FULL JOIN 
+# 	(SELECT to_char(dato, 'YYYY - uge IW') uge,count(*) filmstriben FROM eressourcer.x_filmstriben_forbrug
+# 	 WHERE dato >= '2017-01-01'
+#      GROUP BY uge) i
+# ON i.uge = y.uge
+# ")
+
+mainstream_loan_phys_vs_eres_df <- dbGetQuery(con_dwh, "
+SELECT coalesce(dato,datoen) dato,coalesce(fysiske,0) fysiske,coalesce(filmstriben,0) filmstriben
+FROM
+(select dato, sum(antal) fysiske from cicero.udlaan
+     WHERE gmb = 'm' AND smb='th'
+     group by dato) y
+full outer join
+(select dato::date datoen, count(*) filmstriben from eressourcer.x_filmstriben_forbrug
+        WHERE dato >= '2017-01-01'
+        group by datoen) i
+  ON datoen = dato
+ order by dato
+  ")
+
+# SELECT i.aar,i.uge,fysiske,filmstriben
+# FROM
+# 	(SELECT date_part('year', dato) aar,date_part('week', dato) uge, sum(antal) fysiske FROM cicero.udlaan
+# 	WHERE gmb = 'm' AND smb='th'
+# GROUP BY aar,uge) y
+# FULL JOIN 
+# 	(SELECT date_part('year', dato) aar,date_part('week', dato) uge,count(*) filmstriben FROM eressourcer.x_filmstriben_forbrug
+# 	 WHERE dato >= '2017-01-01'
+#      GROUP BY aar,uge) i
+# ON i.aar = y.aar AND i.uge = y.uge
 
 licenser_overblik <- dbGetQuery(con_dwh, "
 SELECT 
@@ -68,7 +127,8 @@ SELECT
                null::date rapport_dato
                FROM eressourcer.x_pubhub_udlaan
                WHERE extract(year from oprettet) >= 2017
-               group by datakilde,eressource,aar,maaned,kilde,bibliotek,rapport_dato) a")
+               group by datakilde,eressource,aar,maaned,kilde,bibliotek,rapport_dato) a
+")
 
 dbDisconnect(con_dwh)
 
@@ -94,7 +154,7 @@ edatabasesTabPanelUI <- function(id) {
                    tabBox(width = 12,
                           id = "tabset45",
                           # Insert only the follow tab and contents if user belongs to the materialeforum group
-                          tabPanel("Generelt", 
+                          tabPanel("Overblik", 
                                    if ('MATERIALEFORUM' %in% ldap_usergroups) {
                                    fluidRow(
                                      column(12,
@@ -152,7 +212,118 @@ edatabasesTabPanelUI <- function(id) {
                                                                     tags$div(h4(htmlOutput(ns("old_erms_dyn_title_2")))),
                                                                     tags$div(h5(htmlOutput(ns("old_erms_dyn_title_4")))),
                                                                     withSpinner(formattableOutput(ns("old_erms_table")), proxy.height="150px")
-                                                ))))))})))))
+                                                ))))))}
+                                   ),
+                          tabPanel("Mainstreamlicenser", 
+                                   if ('MATERIALEFORUM' %in% ldap_usergroups) {
+                                     fluidRow(
+                                       column(12,
+                                              h2("Mainstreamlicenser"),
+                                              tags$br(),tags$br(),tags$br(),tags$br()
+                                       ),
+                                       column(12,
+                                              column(1,
+                                                     h4("Afgrænsning"),
+                                                     tags$br(),tags$br()
+                                                     # checkboxGroupInput(ns("old_erms_aar"),
+                                                     #                    'År:',
+                                                     #                    sort(unique(as.character(licenser_overblik$aar))),
+                                                     #                    selected = as.character(max(licenser_overblik$aar)),
+                                                     #                    inline = F)
+                                              ),
+                                              column(5,
+                                                      h3("Udgifter, andele"),
+                                                      span("Følgende statistik stammer fra"),a("bibstats.dbc.dk", href = "https://bibstats.dbc.dk", target="_blank" ),
+                                                      withSpinner(plotlyOutput(ns("mainstream_cost_pie"))),
+                                                      tags$br(),tags$br()
+                                              ),
+                                              column(1,
+                                                     h4("Afgrænsning"),
+                                                     tags$br(),tags$br()
+                                              ),
+                                              column(5,
+                                                     h3("Udlån, andele"),
+                                                     span("Følgende statistik stammer fra"),a("bibstats.dbc.dk", href = "https://bibstats.dbc.dk", target="_blank" ),
+                                                     withSpinner(plotlyOutput(ns("mainstream_loan_pie"))),
+                                                     tags$br(),tags$br()
+                                              )
+                                       ),
+                                       column(12,
+                                              column(2,
+                                                     h4("Afgrænsning"),
+                                                     tags$br(),tags$br(),
+                                                     selectInput(ns("phys_vs_eres_periode"),"Periode:", c('dato','uge','maaned','aar')),
+                                                     tags$br(),tags$br(),
+                                                     dateRangeInput(ns('dateRange_phys_vs_eres'),
+                                                                    label = 'Datointerval',
+                                                                    start = Sys.Date() - 182, end = Sys.Date(),
+                                                                    separator = " - "
+                                                     )
+                                                     # ,selectInput(ns("oms_pukt"),"eRessource:",  sort(unique(licenser_overblik$eressource)))
+                                              ),
+                                              column(10,
+                                                     h3("Film udlån, fysiske vs. Filmstriben"),
+                                                     span("Følgende statistik stammer fra"),a("bibstats.dbc.dk", href = "https://bibstats.dbc.dk", target="_blank" ),
+                                                     withSpinner(plotlyOutput(ns("mainstream_loan_phys_vs_eres"))),
+                                                     div(
+                                                       p('N.B! Tal, der stammer fra Faktor (hovedparten), har pt. (sommer 2019) fortsat beta status')
+                                                     )
+                                              )
+                                       ),
+                                       column(12,
+                                              column(2,
+                                                     h4("Afgrænsning"),
+                                                     tags$br(),
+                                                     selectInput(ns("old_erms_produkt2"),"eRessource:",  sort(unique(licenser_overblik$eressource))),
+                                                     checkboxGroupInput(ns("old_erms_aar2"),
+                                                                        'År:',
+                                                                        sort(unique(as.character(licenser_overblik$aar))),
+                                                                        selected = as.character(max(licenser_overblik$aar)),
+                                                                        inline = F),
+                                                     tags$br(),tags$br(),
+                                                     xlsxDownloadUI(ns("licenser_overblik2")),
+                                                     tags$div(HTML('<a id="print-checkouts" class="btn btn-default btn-print" onclick="printDiv.call(this,event,\'.col-sm-12\',\'700px\')"><i class="fa fa-print"></i> Print denne sektion</a>'))
+                                              ),
+                                              column(10,
+                                                     fluidRow(
+                                                       column(10,
+                                                              tags$head(
+                                                                # Style the error message when no year is checked off
+                                                                tags$style(HTML("#shiny-tab-edatabases .shiny-output-error-validation { color: black; font-size: 20px; }"))
+                                                              ),
+                                                              h3("Licenser, udgifter"),
+                                                              span("Følgende statistik stammer fra"),a("bibstats.dbc.dk", href = "https://bibstats.dbc.dk", target="_blank" ),
+                                                              span("samt"),a("Pubhub", href = "https://admin.puhub.dk", target="_blank" )
+                                                       )
+                                                     ),
+                                                     fluidRow(
+                                                       column(10,
+                                                              # Duelling action buttons (only one active at a time, like radio buttons)
+                                                              div(
+                                                                actionButton(ns("yr2"), "Årlig"),
+                                                                actionButton(ns("halvaar2"), "Halvårlig"),
+                                                                actionButton(ns("kvartal2"), "Kvartalsvis"),
+                                                                actionButton(ns("maaned2"), "Månedlig")
+                                                                , style="float:right")
+                                                       )
+                                                     ),
+                                                     fluidRow(
+                                                       column(10,
+                                                              tags$br(),tags$br(),
+                                                              tags$div( h4(htmlOutput(ns("old_erms_dyn_title_1b"))),style = "text-align: center;" ),
+                                                              tags$div( h5(htmlOutput(ns("old_erms_dyn_title_3b"))),style = "text-align: center;" ),
+                                                              withSpinner(plotlyOutput(ns("old_erms_plotb"))),
+                                                              tags$br(),tags$br(),
+                                                              column(12,
+                                                                     tags$div(h4(htmlOutput(ns("old_erms_dyn_title_2b")))),
+                                                                     tags$div(h5(htmlOutput(ns("old_erms_dyn_title_4b")))),
+                                                                     withSpinner(formattableOutput(ns("old_erms_table2")), proxy.height="150px")
+                                                              ))))
+                                              )
+                                       )
+                              }
+                          )
+                  ))))
 }
 
 # SERVER
@@ -242,7 +413,7 @@ edatabasesTabPanel <- function(input, output, session, data, tablename) {
         colNames <- names(old_erms_df())[-1:-6]      # ie. get all colnames except the first thru the sixth
         len <- length(colNames)                      # the number of columns with years
         years <- unique(licenser_overblik$aar)       # get list of the years in the dataframe for assigning persistent colors to the bars
-        aar1 = names(old_erms_df())[7]               # the columns having the data for each year start at column 7
+        aar1 = names(old_erms_df())[7]               # the columns having the data for each year starting at column 7
         h = which(years==aar1)                       # also needed for persistent colors
         
         p <- plot_ly(old_erms_df(),
@@ -316,4 +487,115 @@ edatabasesTabPanel <- function(input, output, session, data, tablename) {
   callModule(xlsxDownload, "licenser_overblik", data = reactive(erms_table_df()), name = "eRessourcer_licenser_overblik")
   
   output$old_erms_table <- renderFormattable({ formattable(erms_table_df()) })
+  
+  
+  ############################################# MAINSTREAM RESSOURCERNE ############################################################
+  
+  mainstream_cost_mattype <- reactive({
+    # Show error if an eRessource is not checked off
+    # validate(
+    #   need(input$old_erms_aar != "", "VÆLG MINDST ÈN RESSOURCE I VENSTREMENUEN")
+    # )
+    mainstream_eres %>%
+    mutate(year = lubridate::year(laan_dato)) %>%
+    group_by(mattype) %>% 
+    summarise(omkostninger = sum(omkostninger)) 
+  })
+  
+  output$mainstream_cost_pie <- renderPlotly({
+    plot_ly(mainstream_cost_mattype(), 
+            labels = ~mattype, 
+            values = ~omkostninger, 
+            text = ~paste(round((omkostninger / sum(omkostninger))*100, 0),"%",sep=""), # denne og følgende linje runder procenterne af, så de er uden decimaler
+            textinfo = 'text',
+            textfont = list(color = '#FFFFFF'), 
+            marker = list(colors = colors, line = list(color = '#FFFFFF', width = 1))) %>%
+      add_pie(hole = 0.6) %>%
+      layout(showlegend = T,
+             xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+             yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+  })
+  
+  mainstream_loan_mattype <- reactive({
+    # Show error if an eRessource is not checked off
+    # validate(
+    #   need(input$old_erms_aar != "", "VÆLG MINDST ÈN RESSOURCE I VENSTREMENUEN")
+    # )
+    mainstream_eres %>%
+      mutate(year = lubridate::year(laan_dato)) %>%
+      group_by(mattype) %>% 
+      summarise(antal = sum(antal)) 
+  })
+  
+  output$mainstream_loan_pie <- renderPlotly({
+    plot_ly(mainstream_loan_mattype(),
+            labels = ~mattype, 
+            values = ~antal, 
+            text = ~paste(round((antal / sum(antal))*100, 0),"%",sep=""), # denne og følgende linje runder procenterne af, så de er uden decimaler
+            textinfo = 'text',
+            textfont = list(color = '#FFFFFF'), 
+            marker = list(colors = colors, line = list(color = '#FFFFFF', width = 1))) %>%
+      add_pie(hole = 0.6) %>%
+      layout(showlegend = T,
+             xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+             yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+  })
+  
+  mainstream_loan_phys_vs_eres_reac <- reactive({
+    mainstream_loan_phys_vs_eres_df %>%
+    mutate(filmstriben = replace_na(filmstriben, 0)) %>%
+    mutate(fysiske = replace_na(fysiske, 0)) %>%
+    filter( dato >= input$dateRange_phys_vs_eres[1] & dato <= input$dateRange_phys_vs_eres[2]) %>%
+    mutate(aar = lubridate::year(dato)) %>%
+    mutate(maaned = lubridate::month(dato)) %>%
+    mutate(uge = lubridate::isoweek(dato)) %>%
+    group_by_(input$phys_vs_eres_periode) %>%
+    summarise(fysiske = sum(fysiske),filmstriben = sum(filmstriben))
+    #spread(key = aar, value = antal) %>%
+    #mutate_at(vars(-1:-6), funs(replace(., is.na(.), 0))) %>%
+    #select(datakilde,eressource,v$data,kilde,bibliotek,rapport_dato,input$old_erms_aar)
+  })
+  
+  output$mainstream_loan_phys_vs_eres <- renderPlotly({
+    plot_ly(mainstream_loan_phys_vs_eres_reac()) %>%
+            add_lines(x = as.formula(paste0("~`", input$phys_vs_eres_periode ,"`")),
+                      y = ~fysiske,
+                      name = 'DVD og Blu-ray',
+                      line = list(color = color3)) %>%
+            add_lines(x = as.formula(paste0("~`", input$phys_vs_eres_periode ,"`")),
+                      y = ~filmstriben,
+                      name = 'Filmstriben',
+                      line = list(color = color4)) %>%
+            layout(
+              #xaxis = list(title="Uge nr.", showgrid = TRUE),
+              xaxis = list(title="Uge nr.", showgrid = TRUE, tickmode='linear'),
+              yaxis = list(
+                tickfont = list(color = color3),
+                title = "Udlån")
+            )
+  })
+  
+  # mainstream_cost_reac <- reactive({
+  #   mainstream_eres %>%
+  #     mutate(filmstriben = replace_na(filmstriben, 0)) %>%
+  #     mutate(fysiske = replace_na(fysiske, 0)) %>%
+  #     filter( dato >= input$dateRange_phys_vs_eres[1] & dato <= input$dateRange_phys_vs_eres[2]) %>%
+  #     mutate(aar = lubridate::year(dato)) %>%
+  #     mutate(maaned = lubridate::month(dato)) %>%
+  #     mutate(uge = lubridate::isoweek(dato)) %>%
+  #     group_by_(input$phys_vs_eres_periode) %>%
+  #     summarise(fysiske = sum(fysiske),filmstriben = sum(filmstriben))
+  #   #spread(key = aar, value = antal) %>%
+  #   #mutate_at(vars(-1:-6), funs(replace(., is.na(.), 0))) %>%
+  #   #select(datakilde,eressource,v$data,kilde,bibliotek,rapport_dato,input$old_erms_aar)
+  # })
+  
+  
+  # output$betjeninger <- renderTable(
+  #   betjeninger <- betjeninger %>% 
+  #     filter(dato > input$queue_date_range[1] & dato < input$queue_date_range[2]) %>%
+  #     mutate(gen_ventetid_min = ifelse(is.na(gen_ventetid_min), 0, gen_ventetid_min), gen_behandlingstid_min = ifelse(is.na(gen_behandlingstid_min), 0, gen_behandlingstid_min)) %>%
+  #     group_by(kø) %>%
+  #     summarise('Antal betjeninger' = sum(antal), 'Antal unikke betjeninger' = sum(unik_antal), 'Gennemsnitlig ventetid (min.)' = mean(gen_ventetid_min), 'Gennemsnitlig betjeningstid (min.)' = mean(gen_behandlingstid_min))
+  # )
 }
